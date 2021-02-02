79b626c29b97cb55c4835337bd31f2d1c6e7ff03
Shane Walker <shane@oak.works>
2021-02-02 15:28:19 -0600

    reverted image upload back to blog, previewing problem.

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
+import uuid from 'react-uuid'
 
 export const ImageUpload = async (file) => {
   let imageUrl = ''
-  const filename = Date.now();
-  const photo = new File([file], filename);
+  const filename = uuid();
   const storageRef = firebase.storage().ref().child(`images/${filename}`);
-  return storageRef.put(photo).then(snapshot => {
+  return storageRef.put(file).then(snapshot => {
     return storageRef.getDownloadURL().then((url) => {
       imageUrl = url
       return imageUrl
