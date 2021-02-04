778e9bd1fd1216581941aa86a9a129c7a6a477c2
Shane Walker <shane@oak.works>
2021-02-04 16:03:02 -0600

    improved styling of submitted and remove close button.

--- src/components/Submitted.js
+++ src/components/Submitted.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import Fab from '@material-ui/core/Fab';
-import CheckCircleOutlineOutlinedIcon from '@material-ui/icons/CheckCircleOutlineOutlined';
+import React from "react";
+import CheckCircleOutlineOutlinedIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
 
 //Local Impots
 
 const Submitted = () => {
   return (
-    <div className='Submitted' style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }} >
-      <div>
-        <h2>We're On it!</h2>
-        <CheckCircleOutlineOutlinedIcon style={{ fontSize: '70px', color:'black' }}/>
-        <p style={{ padding: '0 2rem' }}>Your request has been submitted. We will reach out to you once we have an Estimate put together.</p>
-        <div style={{marginTop: '65%'}}>
-          <Fab color="primary" size="large" href='http://localhost:3000' variant="extended" aria-label="add">
-            close
-        </Fab>
-          <p style={{color: 'gray', fontSize: '.50rem' }}>You can close this window or head back to our website.</p>
+    <div
+      className="Submitted"
+      style={{height: '90vh'}}
+    >
+      <div style={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", textAlign: "center" }}>
+        <div style={{marginTop: "10%"}}>
+          <h2>We're On it!</h2>
+          <CheckCircleOutlineOutlinedIcon
+            style={{ fontSize: "70px", color: "black" }}
+          />
+          <p style={{ padding: "0 2rem" }}>
+            Your request has been submitted. We will reach out to you once we
+            have an Estimate put together.
+          </p>
+        </div>
+        <div>
+          <p style={{ color: "gray", fontSize: "1.00rem", paddingRight: "1rem", paddingLeft: "1rem" }}>
+            You can close this window or head back to our website.
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Submitted
+export default Submitted;
