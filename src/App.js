2f7a10c66ebd005f9069d2eb5de69e52d7ea5aa8
Shane Walker <shane@oak.works>
2021-02-11 05:44:39 -0600

    abstracted header image to component

--- src/App.js
+++ src/App.js
@@ -6,20 +6,6 @@ import "./components/styles/Overrides.css";
 function App() {
   return (
     <div>
-      <div
-        style={{
-          backgroundColor: "black",
-          height: "5rem",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <img
-          alt=""
-          style={{ maxHeight: "-webkit-fill-available" }}
-          src="https://img1.wsimg.com/isteam/ip/00169b59-da67-4bbf-9c7c-e656dba9028e/finelinewhite.png"
-        ></img>
-      </div>
       <EstimateForm />
     </div>
   );
