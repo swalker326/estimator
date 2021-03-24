e4efa45f7ff260e043bfcfbd440b4f737e4484dd
Shane <shane@steveross.io>
2021-03-24 07:56:58 -0500

    adding coming soon component to deploy.

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router";
 import PayPalBtn from "./PayPalBtn";
+import ComingSoon from "./ComingSoon";
 
 //Local Impots
 
 const Splash = () => {
   const [subscribed, setSubscribed] = useState(false);
-  
+
   const paypalSubscribe = (data, actions) => {
     return actions.subscription.create({
       plan_id: process.env.REACT_APP_PAYPAL_PLAN_ID,
@@ -19,10 +20,10 @@ const Splash = () => {
     // call the backend api to store transaction details
     setSubscribed(true);
   };
-  if (subscribed) return <Redirect to="/login"/>
+  if (subscribed) return <Redirect to="/login" />;
   return (
     <div className="Splash">
-      <h1>Welcome to a super cool splace screen! YAY!</h1>
+      {/* <h1>Welcome to a super cool splace screen! YAY!</h1>
       <PayPalBtn
         amount="29.99"
         currency="USD"
@@ -31,10 +32,10 @@ const Splash = () => {
         catchError={paypalOnError}
         onError={paypalOnError}
         onCancel={paypalOnError}
-      />
+      /> */}
+      <ComingSoon />
     </div>
   );
 };
 
 export default Splash;
-
