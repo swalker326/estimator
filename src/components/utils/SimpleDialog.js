4a3824efd8893d5d989d4f8669132c01fbc17cd4
Shane <shane@steveross.io>
2021-04-01 11:51:16 -0500

    add icon color. Remove inline styles.

--- src/components/utils/SimpleDialog.js
+++ src/components/utils/SimpleDialog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
@@ -7,43 +7,43 @@ import Dialog from "@material-ui/core/Dialog";
 
 import { blue } from "@material-ui/core/colors";
 
-const useStyles = makeStyles({
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-  root: {
-    marginRight: "2rem",
-    marginBottom: "2rem",
-    marginLeft: "2rem",
-  },
-  buttonGroup: {
-    display: "flex",
-    marginTop: "2rem",
-    justifyContent: "space-between"
-  }
-});
+
 
 function SimpleDialog(props) {
+  const [deleteRequest, setDeleteRequest] = useState(false);
   const classes = useStyles();
-  const { onClose, open } = props;
+  const { onClose, open, deleteCurrentRequest, setOpen } = props;
 
   const handleClose = () => {
-    onClose();
+    
   };
+
+  useEffect(() => {
+    if (deleteRequest) {
+      deleteCurrentRequest();
+      onClose();
+    } else {
+      setOpen(false);
+    }
+    setOpen(false)
+  },[deleteRequest])
 
   return (
     <Dialog
-      onClose={handleClose}
+      onClose={() => setOpen(false)}
       aria-labelledby="simple-dialog-title"
       open={open}
     >
       <DialogTitle id="simple-dialog-title">Confirm Request</DialogTitle>
-      <div className= {classes.root}>
+      <div className={classes.root}>
         <div>Are you sure you would like to delete the request</div>
         <div className={classes.buttonGroup}>
-          <Button variant= "outlined" color="primary">No Go Back</Button>
-          <Button variant= "contained" onClick={handleClose} color="secondary">Yes Delete</Button>
+          <Button variant="outlined" onClick={() => setOpen(false)} color="primary">
+            No Go Back
+          </Button>
+          <Button variant="contained" onClick={() => setDeleteRequest(true)} color="secondary">
+            Yes Delete
+          </Button>
         </div>
       </div>
     </Dialog>
@@ -53,6 +53,7 @@ function SimpleDialog(props) {
 SimpleDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired,
 };
 
 export default function SimpleDialogDemo(props) {
@@ -68,13 +69,25 @@ export default function SimpleDialogDemo(props) {
 
   return (
     <div>
-      <Button onClick={handleClickOpen}>
-        {<props.icon />}
-      </Button>
-      <SimpleDialog
-        open={open}
-        onClose={handleClose}
-      />
+      <Button onClick={handleClickOpen}>{<props.icon style={{color: `${props.iconColor}`}}/>}</Button>
+      <SimpleDialog open={open} deleteCurrentRequest={props.deleteRequest} setOpen={setOpen} onClose={handleClose} setOpen={setOpen}  />
     </div>
   );
 }
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600],
+  },
+  root: {
+    marginRight: "2rem",
+    marginBottom: "2rem",
+    marginLeft: "2rem",
+  },
+  buttonGroup: {
+    display: "flex",
+    marginTop: "2rem",
+    justifyContent: "space-between",
+  },
+});
